9567f1952f67680d7118d89cca668e8d8d3ad9bb
ntwari-egide <ntwariegide2@gmail.com>
2022-05-10 16:07:37 +0200

    fix bug in icons

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -58,5 +58,10 @@
 @endforeach
 </table>
 {!! $companies->links() !!}
+
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 </body>
 </html>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,6 @@
 <div class="bg-green-800 p-8 text-center text-white flex">
-    <i class="fa-thin fa-magnifying-glass"></i>
+    <p class="mr-8 text-xl">
+        <ion-icon name="search-outline"></ion-icon>
+    </p>
     <h1>content goes here</h1>
 </div>
